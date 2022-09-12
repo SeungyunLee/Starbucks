d07ac5b0a8c8bde204b0312a08b43160c8b00939
SeungyunLee <seungyun.dev@gmail.com>
2022-09-12 18:17:37 +0900

    2022.12.9 수정

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.scrollY);
+  
   if (window.scrollY > 500) {
     //배지 숨기기
     // gsap.to(요소, 지속시간, 옵션);
@@ -32,7 +32,7 @@ window.addEventListener('scroll', _.throttle(function () {
       display: 'none'
     });
     //버튼 보이기!
-    gsap.to('#to-top', .2, {
+    gsap.to(toTopEl, .2, {
       x:0
     });
 
@@ -43,7 +43,7 @@ window.addEventListener('scroll', _.throttle(function () {
       display: 'block'
     });
     //버튼 숨기기!
-    gsap.to('#to-top', .2, {
+    gsap.to(toTopEl, .2, {
       x: 100
     });
   }
@@ -105,6 +105,7 @@ new Swiper('.awards .swiper', {
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
+
 promotionToggleBtn.addEventListener('click', function () {
   isHidePromotion =!isHidePromotion
   if (isHidePromotion) {
@@ -116,6 +117,8 @@ promotionToggleBtn.addEventListener('click', function () {
   }
 });
 
+
+// 부유하는 요소
 
 // 범위 랜덤 함수(소수점 2자리까지)
 function random(min, max) {
